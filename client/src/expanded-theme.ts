57b6f98cbd645e4e2937144e45c879315489e339
Dillan Scott <dillan.scott22@imperial.ac.uk>
2024-12-22 19:40:22 +0000

    feat: add basic frontend for dashboard page

---
+++ client/src/expanded-theme.ts
@@ -0,0 +1,13 @@
+// TODO: remove import?
+//eslint-disable-next-line
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette" {
+  interface PaletteColor {
+    [key: number]: string;
+  }
+
+  interface Palette {
+    tertiary: PaletteColor;
+  }
+}
---
+++ client/src/scenes/dashboard/Row2.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardBox from "../../components/DashboardBox";
+
+const Row2 = () => {
+  return (
+    <>
+      <DashboardBox gridArea="d"></DashboardBox>
+      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="f"></DashboardBox>
+    </>
+  );
+};
+
+export default Row2;
