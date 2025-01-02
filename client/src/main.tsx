2651f9031cfc7808dd3fc5131a8d1498bda48792
Dillan Scott <dillan.scott22@imperial.ac.uk>
2025-01-02 16:58:29 +0000

    feat: create and seed mongo db and setup data routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "./state/api";
+
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
